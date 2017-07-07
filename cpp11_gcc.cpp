3fce2ea4a85cf335c407eb3fe24bb244978ed4a6
Ryu Wakimoto <ryu.wakimoto.s7@dc.tohoku.ac.jp>
2017-07-07 13:57:55 +0900

    Initial commit

---
+++ cpp11_gcc.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define MOD 1000000007
+#define INTMAX 0x7fffffff
+#define INTMIN 0x80000000
+#define LLMAX 0x7fffffffffffffffLL
+#define LLMIN 0x8000000000000000LL
+#define EPS 1e-9
+
+#define REP(i,a,b) for(int i=a;i<b;i++)
+#define rep(i,n) REP(i,0,n)
+
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main(){
+
+}
